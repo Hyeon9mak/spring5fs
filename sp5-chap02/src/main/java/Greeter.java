dac3f1adba7853be2f54b72b3d3757cb54fb1dca
hyeon9mak <jinha3507@gmail.com>
2021-05-02 16:48:26 +0900

    feat: add chap02 create bean, get bean code

---
+++ sp5-chap02/src/main/java/Greeter.java
@@ -0,0 +1,12 @@
+public class Greeter {
+
+    private String format;
+
+    public String greet(String guest) {
+        return String.format(format, guest);
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+}
